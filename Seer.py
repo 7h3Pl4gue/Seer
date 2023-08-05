5582439b4e03f26a6b92831a781cff0558ff9603
R4GN4R0K__ <101607846+Alt0fCtrl@users.noreply.github.com>
2023-08-05 11:07:29 -0400

    Add files via upload

--- Seer.py
+++ Seer.py
@@ -1,7 +1,8 @@
-# import pyfiglet and colorama modules
 import pyfiglet
 from colorama import init, Fore, Style
 import requests
+import codecs
+import re
 
 # Initialize colorama
 init()
@@ -11,20 +12,26 @@ def print_ascii_art(text):
     result = pyfiglet.figlet_format(text)
     print(result)
 
+# Function to check if a subdomain is valid
+def is_valid_subdomain(subdomain):
+    # Using regex to check if the subdomain contains only valid characters (alphanumeric and hyphen)
+    return re.match(r'^[a-zA-Z0-9\-]+$', subdomain) is not None
+
 # Function for scanning subdomains
 def domain_scanner(domain_name, subdomain_list, directory_list):
     print('----URLs after scanning----')
 
     for subdomain in subdomain_list:
-        url = f"https://{subdomain}.{domain_name}"
-        try:
-            response = requests.get(url)
-            if response.status_code == 200:
-                print(f'[+] {Fore.GREEN}{url} - Live (Status Code: {response.status_code}){Style.RESET_ALL}')
-            else:
-                print(f'[-] {Fore.RED}{url} - Not Live (Status Code: {response.status_code}){Style.RESET_ALL}')
-        except requests.ConnectionError:
-            print(f'[-] {Fore.RED}{url} - Connection Error{Style.RESET_ALL}')
+        if subdomain and is_valid_subdomain(subdomain):
+            url = f"https://{subdomain}.{domain_name}"
+            try:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    print(f'[+] {Fore.GREEN}{url} - Live (Status Code: {response.status_code}){Style.RESET_ALL}')
+                else:
+                    print(f'[-] {Fore.RED}{url} - Not Live (Status Code: {response.status_code}){Style.RESET_ALL}')
+            except requests.ConnectionError:
+                print(f'[-] {Fore.RED}{url} - Connection Error{Style.RESET_ALL}')
 
         for directory in directory_list:
             url = f"https://{subdomain}.{domain_name}/{directory}"
@@ -53,16 +60,22 @@ if __name__ == '__main__':
         # Input the word list filename for subdomains
         subdomain_word_list_file = input("Enter the Subdomain Word List Filename: ")
 
-        # Opening the subdomain word list file
-        with open(subdomain_word_list_file, 'r') as file:
-            # Reading the file and splitting it into lines
-            subdomain_word_list = file.read().splitlines()
+        # Attempt to open the subdomain word list file with 'utf-8' encoding
+        try:
+            with codecs.open(subdomain_word_list_file, 'r', encoding='utf-8') as file:
+                # Reading the file and splitting it into lines
+                subdomain_word_list = file.read().splitlines()
+        except UnicodeDecodeError:
+            # If 'utf-8' encoding fails, try 'latin-1' encoding
+            with codecs.open(subdomain_word_list_file, 'r', encoding='latin-1') as file:
+                # Reading the file and splitting it into lines
+                subdomain_word_list = file.read().splitlines()
 
         # Input the word list filename for directories
         directory_word_list_file = input("Enter the Directory Word List Filename: ")
 
         # Opening the directory word list file
-        with open(directory_word_list_file, 'r') as file:
+        with codecs.open(directory_word_list_file, 'r', encoding='utf-8') as file:
             # Reading the file and splitting it into lines
             directory_word_list = file.read().splitlines()
 
@@ -75,10 +88,17 @@ if __name__ == '__main__':
         # Input the word list filename for subdomains
         subdomain_word_list_file = input("Enter the Subdomain Word List Filename: ")
 
-        # Opening the subdomain word list file
-        with open(subdomain_word_list_file, 'r') as file:
-            # Reading the file and splitting it into lines
-            subdomain_word_list = file.read().splitlines()
+        # Attempt to open the subdomain word list file with 'utf-8' encoding
+        try:
+            with codecs.open(subdomain_word_list_file, 'r', encoding='utf-8') as file:
+                # Reading the file and splitting it into lines
+                subdomain_word_list = file.read().splitlines()
+        except UnicodeDecodeError:
+            # If 'utf-8' encoding fails, try 'latin-1' encoding
+            with codecs.open(subdomain_word_list_file, 'r', encoding='latin-1') as file:
+                # Reading the file and splitting it into lines
+                subdomain_word_list = file.read().splitlines()
 
         # Calling the function for scanning the subdomains
-        domain_scanner(dom_name, subdomain_word_list, [])
+        domain_scanner(dom_name, subdomain_word_list, []) 
+
